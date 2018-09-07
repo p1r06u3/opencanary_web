1cf40a7d781ce0ec9a022452bff85970a84c92dd
p1r06u3 <p1r06u3@gmail.com>
2018-09-07 14:17:55 +0800

    Add job woker

---
+++ util/task.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+  Author: pirogue 
+  Purpose: 计划任务模块
+  Site: http://pirogue.org 
+  Created: 2018-02-08 11:18:10
+"""
+
+
+from apscheduler.schedulers.tornado import TornadoScheduler
+sched = TornadoScheduler()
