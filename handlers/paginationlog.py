5de829a4fabf2080187f1e274b7d1b6ee9c937a2
p1r06u3 <p1r06u3@gmail.com>
2018-12-25 20:12:49 +0800

    Fixed pagination bug

--- handlers/paginationlog.py
+++ handlers/paginationlog.py
@@ -10,7 +10,7 @@
 
 import tornado
 import json
-from service.paginationlog import listpage
+from service.paginationlog import listpage, total_atk_page, total_wit_page
 from base import BaseHandler
 from util.auth import jwtauth
 
@@ -32,7 +32,7 @@ class GetlistJsonHandler(BaseHandler):
             self.send_error(status_code=400) # 向浏览器发送错误状态码，会调用write_error
 
         param = self.request.body.decode('utf-8')
-        print 'page start'
+        # print 'page start'
 
         # print type(param)
         
@@ -42,7 +42,12 @@ class GetlistJsonHandler(BaseHandler):
         # print param
         # print(type(param))
         self.write(viewres)
-    
-    def get(self):
-        self.write('get ok')
 
+    def get(self):
+        # 分页列表
+        types = self.get_argument('type')
+        if types:
+            if int(types) == 1:
+                self.write(str(total_wit_page()))
+            elif int(types) == 2:
+                self.write(str(total_atk_page()))
